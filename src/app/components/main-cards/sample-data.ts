dffa9cc75520d8dd4c2b92c2e849bb91ab21c7f6
AliZoghi <alizoghiwork@gmail.com>
2022-07-24 16:50:09 +0430

    [FIX,OPTIMIZE] pr team3, image

--- src/app/components/main-cards/sample-data.ts
+++ src/app/components/main-cards/sample-data.ts
@@ -1,13 +1,13 @@
 import {Product} from './models/product';
 
 export const Products: Product[] = [
-    {oldPrice: '60000', newPrice: '46000', imageUrl: 'assets/images/card1.avif'},
-    {oldPrice: '40000', newPrice: '24000', imageUrl: 'assets/images/card2.avif'},
-    {oldPrice: '28000', newPrice: '20000', imageUrl: 'assets/images/card3.avif'},
-    {oldPrice: '23000', newPrice: '14000', imageUrl: 'assets/images/card4.avif'},
-    {oldPrice: '90000', newPrice: '78000', imageUrl: 'assets/images/card5.avif'},
-    {oldPrice: '95000', newPrice: '90000', imageUrl: 'assets/images/card6.avif'},
-    {oldPrice: '76000', newPrice: '60000', imageUrl: 'assets/images/card7.avif'},
-    {oldPrice: '35000', newPrice: '30000', imageUrl: 'assets/images/card8.avif'},
-    {oldPrice: '25000', newPrice: '14000', imageUrl: 'assets/images/card9.avif'},
+    {oldPrice: '60000', newPrice: '46000', imageUrl: 'assets/images/card1.webp'},
+    {oldPrice: '40000', newPrice: '24000', imageUrl: 'assets/images/card2.webp'},
+    {oldPrice: '28000', newPrice: '20000', imageUrl: 'assets/images/card3.webp'},
+    {oldPrice: '23000', newPrice: '14000', imageUrl: 'assets/images/card4.webp'},
+    {oldPrice: '90000', newPrice: '78000', imageUrl: 'assets/images/card5.webp'},
+    {oldPrice: '95000', newPrice: '90000', imageUrl: 'assets/images/card6.webp'},
+    {oldPrice: '76000', newPrice: '60000', imageUrl: 'assets/images/card7.webp'},
+    {oldPrice: '35000', newPrice: '30000', imageUrl: 'assets/images/card8.webp'},
+    {oldPrice: '25000', newPrice: '14000', imageUrl: 'assets/images/card9.webp'},
 ];
