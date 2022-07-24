c8c1608cc1dc44dac47d74419759dcc1ad727560
arefhbb <haref3460@gmail.com>
2022-07-24 13:47:46 +0430

    [FIX] some bugs

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -5,8 +5,4 @@ import {Component, OnInit} from '@angular/core';
     templateUrl: './footer.component.html',
     styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent implements OnInit {
-    constructor() {}
-
-    ngOnInit(): void {}
-}
+export class FooterComponent {}
