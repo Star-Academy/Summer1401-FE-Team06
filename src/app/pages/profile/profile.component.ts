12f30cc26fcaded454c8984cd04f2ee93dc26fa1
AliZoghi <alizoghiwork@gmail.com>
2022-07-27 09:42:53 +0430

    [FIX] pr sina

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -5,4 +5,9 @@ import {Component, OnInit} from '@angular/core';
     templateUrl: './profile.component.html',
     styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+    public logoutButtonClickHandler(): void {
+        localStorage.removeItem('token');
+        location.reload();
+    }
+}
--- src/app/utils/api.utils.ts
+++ src/app/utils/api.utils.ts
@@ -1,10 +1,8 @@
 export const BASE_URL = 'https://games.bijanprogrammer.com';
 
-export const API_USER_ONE = BASE_URL + '/user/one';
 export const API_USER_AUTH = BASE_URL + '/user/auth';
 export const API_USER_LOGIN = BASE_URL + '/user/login';
 export const API_USER_REGISTER = BASE_URL + '/user/register';
-export const API_USER_ALTER = BASE_URL + '/user/alter';
 
 export const POST_REQUEST_INIT: RequestInit = {
     method: 'post',
