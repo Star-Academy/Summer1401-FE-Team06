0be2d5e3a91dd807334587c8ea5fb62c793edb8e
arefhbb <haref3460@gmail.com>
2022-08-02 02:59:21 +0430

    [CHANGE] banner ui

--- src/app/components/main-cards/card/card.component.ts
+++ src/app/components/main-cards/card/card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, Output} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Product} from '../models/product';
 
 @Component({
--- src/app/components/main-cards/main-cards.component.spec.ts
+++ src/app/components/main-cards/main-cards.component.spec.ts
@@ -5,5 +5,41 @@ import {CommonModule} from '@angular/common';
 import {CardModule} from './card/card.module';
 import {PrimaryTitleModule} from '../primary-title/primary-title.module';
 import {MoreButtonModule} from '../more-button/more-button.module';
+import {CardComponent} from './card/card.component';
 
-describe('MainCardsComponent', () => {});
+describe('MainCardsComponent', () => {
+    let component: MainCardsComponent;
+    let fixture: ComponentFixture<MainCardsComponent>;
+    let host: HTMLElement;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [MainCardsComponent, CardComponent],
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MainCardsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+        host = fixture.nativeElement as HTMLElement;
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('tests - cards', () => {
+        let appCards = host.querySelectorAll('app-card');
+        fixture.detectChanges();
+        console.log(appCards[0].querySelector('img'));
+        expect(appCards).toBeTruthy();
+        //for (let {card, i} of appCards as any) {
+        //expect(card.querySelector('img').src).toEqual(component.cards[i].imageUrl);
+        //expect(+card.querySelector('.old-price').textContent).toEqual(component.cards[i].oldPrice);
+        //expect(+card.querySelector('.new-price').textContent).toEqual(component.cards[i].newPrice);
+        //}
+    });
+
+
+});
