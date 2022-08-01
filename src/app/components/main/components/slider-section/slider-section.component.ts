51233c47a098de5218948ba87ac7dff0214c3f20
AliZoghi <alizoghiwork@gmail.com>
2022-08-01 17:26:34 +0430

    [EDIT] slide card

--- src/app/components/main/components/slider-section/slider-section.component.ts
+++ src/app/components/main/components/slider-section/slider-section.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {Products} from '../../../main-cards/sample-data';
 import {Product} from '../../../main-cards/models/product';
 
@@ -9,4 +9,20 @@ import {Product} from '../../../main-cards/models/product';
 })
 export class SliderSectionComponent {
     public cards: Product[] = Products;
+    @Input() public isReverse: boolean = false;
+
+    @ViewChild('slider') public sliderContainer!: ElementRef;
+    public scrollToLeft(): void {
+        this.scrollFunction(300);
+    }
+
+    public scrollToRight(): void {
+        this.scrollFunction(-300);
+    }
+    public scrollFunction(width: number): void {
+        this.sliderContainer.nativeElement.scrollTo({
+            left: this.sliderContainer.nativeElement.scrollLeft + width,
+            behavior: 'smooth',
+        });
+    }
 }
