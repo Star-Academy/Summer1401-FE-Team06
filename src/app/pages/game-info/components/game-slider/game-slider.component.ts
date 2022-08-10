302e5e92d52fd586a3b7c97d8ffc3de1574f9d12
AliZoghi <alizoghiwork@gmail.com>
2022-08-10 17:14:06 +0430

    [ADD] game-slider component

---
+++ src/app/pages/game-info/components/game-slider/game-slider.component.ts
@@ -0,0 +1,69 @@
+import {AfterViewInit, Component, ElementRef, OnDestroy, ViewChild} from '@angular/core';
+
+interface Item {
+    src: string;
+}
+@Component({
+    selector: 'app-game-slider',
+    templateUrl: './game-slider.component.html',
+    styleUrls: ['./game-slider.component.scss'],
+})
+export class GameSliderComponent implements AfterViewInit, OnDestroy {
+    public readonly INTERVAL_DELAY: number = 4_000;
+
+    public items: Item[] = [
+        {src: 'assets/images/slider3.webp'},
+        {src: 'assets/images/slider1.webp'},
+        {src: 'assets/images/slider4.webp'},
+    ];
+
+    public cardWidthConstant: number = 300;
+
+    @ViewChild('slider') public sliderContainer!: ElementRef;
+    public scrollToLeft(): void {
+        this.scrollFunction(this.cardWidthConstant);
+    }
+
+    public scrollToRight(): void {
+        this.scrollFunction(-this.cardWidthConstant);
+    }
+    public scrollFunction(width: number): void {
+        this.sliderContainer.nativeElement.scrollTo({
+            left: this.sliderContainer.nativeElement.scrollLeft + width,
+            behavior: 'smooth',
+        });
+    }
+
+    public activeIndex: number = 0;
+
+    public interval!: number;
+
+    public ngAfterViewInit(): void {
+        this.resetInterval();
+    }
+
+    public ngOnDestroy(): void {
+        clearInterval(this.interval);
+    }
+
+    public changeActiveIndex(index: number): void {
+        if (index < 0) index = this.items.length - 1;
+        else if (index >= this.items.length) index = 0;
+
+        this.activeIndex = index;
+        this.resetInterval();
+    }
+
+    public resetInterval(): void {
+        clearInterval(this.interval);
+
+        this.interval = setInterval(() => {
+            this.changeActiveIndex(this.activeIndex + 1);
+        }, this.INTERVAL_DELAY);
+    }
+
+    public activeWithClickBullet(index: number): void {
+        this.activeIndex = index;
+        this.resetInterval();
+    }
+}
--- src/app/pages/game-info/game-info.module.ts
+++ src/app/pages/game-info/game-info.module.ts
@@ -5,9 +5,10 @@ import {HeaderModule} from '../../components/header/header.module';
 import {FooterModule} from '../../components/footer/footer.module';
 import {SectionModule} from '../../components/section/section.module';
 import {GameInfoComponent} from './game-info.component';
+import { GameSliderComponent } from './components/game-slider/game-slider.component';
 
 @NgModule({
-    declarations: [GameInfoComponent],
+    declarations: [GameInfoComponent, GameSliderComponent],
     imports: [CommonModule, HeaderModule, FooterModule, SectionModule],
 })
 export class GameInfoModule {}
