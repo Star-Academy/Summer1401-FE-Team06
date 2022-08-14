c7db6e9f758425d16f6d317b4109ff4314d086c4
AliZoghi <alizoghiwork@gmail.com>
2022-08-14 15:44:26 +0430

    [REFACTOR] navbar component

--- src/app/components/header/components/first-navbar/first-navbar.component.ts
+++ src/app/components/header/components/first-navbar/first-navbar.component.ts
@@ -1,5 +1,8 @@
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
+import {IconColor} from 'src/app/enum/icon-color.enum';
+import {IconType} from 'src/app/enum/icon-type.enum';
+import {AuthService} from '../../../../services/auth.service';
 
 @Component({
     selector: 'app-first-navbar',
@@ -7,8 +10,18 @@ import {Router} from '@angular/router';
     styleUrls: ['./first-navbar.component.scss'],
 })
 export class FirstNavbarComponent {
-    public constructor(private router: Router) {}
+    public isLoggedIn: boolean = false;
+    public IconType = IconType;
+    public IconColor = IconColor;
+
+    public constructor(private router: Router, private authService: AuthService) {
+        this.initializeVariables().then();
+    }
     public async submitSearchPhrase(sentence: string): Promise<void> {
         await this.router.navigate(['/search'], {queryParams: {searchPhrase: sentence}});
+    }
+
+    private async initializeVariables(): Promise<void> {
+        this.isLoggedIn = await this.authService.isLoggedIn();
     }
 }
